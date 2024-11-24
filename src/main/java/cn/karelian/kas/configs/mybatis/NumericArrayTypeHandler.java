e8d9c1ff968a24804804c50c5097457cdf7bbee9
Karelian-na <karelian_na@163.com>
2024-11-25 00:14:51 +0800

    refactor: Unify the typeHandler of numeric arrays, use `NumbericArrayTypeHandler` directly.

--- src/main/java/cn/karelian/kas/configs/mybatis/NumericArrayTypeHandler.java
+++ src/main/java/cn/karelian/kas/configs/mybatis/NumericArrayTypeHandler.java
@@ -1,46 +1,59 @@
 package cn.karelian.kas.configs.mybatis;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.Method;
 import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
 import org.apache.ibatis.type.BaseTypeHandler;
 import org.apache.ibatis.type.JdbcType;
 
-public abstract class NumericArrayTypeHandler<T extends Number> extends BaseTypeHandler<T[]> {
+import lombok.extern.slf4j.Slf4j;
 
-	public static class ByteArray extends NumericArrayTypeHandler<Byte> {
-		@Override
-		protected Byte[] parseFrom(String[] str) {
-			return Stream.of(str).map(Byte::valueOf).toArray(Byte[]::new);
+@Slf4j
+public class NumericArrayTypeHandler<T extends Number> extends BaseTypeHandler<T[]> {
+	private static final List<Class<?>> numericClasses = Arrays.asList(Byte.class, Short.class, Integer.class,
+			Long.class, Double.class, Float.class);
+
+	private Class<?> type;
+
+	public NumericArrayTypeHandler(Class<T[]> type) {
+		if (type == null) {
+			throw new IllegalArgumentException("Type argument cannot be null");
+		}
+
+		this.type = type.getComponentType();
+
+		if (!numericClasses.contains(this.type)) {
+			throw new IllegalArgumentException(
+					"Non primitive type argument cannot be annotated with `NumericArrayTypeHandler` to handler resultset!");
 		}
 	}
 
-	public static class ShortArray extends NumericArrayTypeHandler<Short> {
-		@Override
-		protected Short[] parseFrom(String[] str) {
-			return Stream.of(str).map(Short::valueOf).toArray(Short[]::new);
+	@SuppressWarnings({ "unchecked" })
+	private T[] parseFromString(String value) {
+		if (null == value) {
+			return null;
+		}
+
+		try {
+			final Method method = type.getMethod("valueOf", String.class);
+			return Stream.of(value.split(",")).map(v -> {
+				try {
+					return method.invoke(null, v);
+				} catch (Exception e) {
+					return null;
+				}
+			}).toArray(size -> (T[]) Array.newInstance(type, size));
+		} catch (Exception e) {
+			return null;
 		}
 	}
-
-	public static class IntegerArray extends NumericArrayTypeHandler<Integer> {
-		@Override
-		protected Integer[] parseFrom(String[] str) {
-			return Stream.of(str).map(Integer::valueOf).toArray(Integer[]::new);
-		}
-	}
-
-	public static class LongArray extends NumericArrayTypeHandler<Long> {
-		@Override
-		protected Long[] parseFrom(String[] str) {
-			return Stream.of(str).map(Long::valueOf).toArray(Long[]::new);
-		}
-	}
-
-	protected abstract T[] parseFrom(String[] str);
 
 	@Override
 	public void setNonNullParameter(PreparedStatement ps, int i, T[] parameter, JdbcType jdbcType)
@@ -51,18 +64,18 @@ public abstract class NumericArrayTypeHandler<T extends Number> extends BaseType
 	@Override
 	public T[] getNullableResult(ResultSet rs, String columnName) throws SQLException {
 		String value = rs.getString(columnName);
-		return value == null ? null : parseFrom(value.split(","));
+		return this.parseFromString(value);
 	}
 
 	@Override
 	public T[] getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
 		String value = rs.getString(columnIndex);
-		return value == null ? null : parseFrom(value.split(","));
+		return this.parseFromString(value);
 	}
 
 	@Override
 	public T[] getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
 		String value = cs.getString(columnIndex);
-		return value == null ? null : parseFrom(value.split(","));
+		return this.parseFromString(value);
 	}
 }
